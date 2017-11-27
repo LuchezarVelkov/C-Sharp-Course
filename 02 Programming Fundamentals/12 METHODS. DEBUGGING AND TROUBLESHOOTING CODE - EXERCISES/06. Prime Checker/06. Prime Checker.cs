a85ff527f77fbb1e1ec807611ad461d78ab2afc6
LuchezarVelkov <luchezar.velk@gmail.com>
2017-11-28 01:10:06 +0200

    Add some homework`s

--- 02 Programming Fundamentals/12 METHODS. DEBUGGING AND TROUBLESHOOTING CODE - EXERCISES/06. Prime Checker/06. Prime Checker.cs
+++ 02 Programming Fundamentals/12 METHODS. DEBUGGING AND TROUBLESHOOTING CODE - EXERCISES/06. Prime Checker/06. Prime Checker.cs
@@ -6,7 +6,34 @@ namespace _06.Prime_Checker
     {
         static void Main(string[] args)
         {
+            long readNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine(ItIsPrime(readNumber));
+        }
 
+        static bool ItIsPrime(long readNumber)
+        {
+            bool isPrime = true;
+            if (readNumber <= 1)
+            {
+                isPrime = false;
+            }
+            if (readNumber % 2 == 2 && readNumber != 2) // 2 is only even prime number.
+            {
+                isPrime = false;
+            }
+            if (isPrime) // for speed optimization, if readNumber is odd can`t be Prime.
+            {
+                for (long i = 2; i <= readNumber - 1; i++)
+                {
+                    if (readNumber % i == 0) // one number is prime if can 
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            //string result = isPrime ? "Prime" : "Not Prime";
+            return isPrime;
         }
     }
 }
