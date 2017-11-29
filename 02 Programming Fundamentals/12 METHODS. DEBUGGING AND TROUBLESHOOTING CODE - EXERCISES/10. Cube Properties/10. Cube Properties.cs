610e46f65fb4129c01a2384414e3a45264d2cdd6
LuchezarVelkov <luchezar.velk@gmail.com>
2017-11-30 01:39:00 +0200

    add some homework`s

--- 02 Programming Fundamentals/12 METHODS. DEBUGGING AND TROUBLESHOOTING CODE - EXERCISES/10. Cube Properties/10. Cube Properties.cs
+++ 02 Programming Fundamentals/12 METHODS. DEBUGGING AND TROUBLESHOOTING CODE - EXERCISES/10. Cube Properties/10. Cube Properties.cs
@@ -6,7 +6,48 @@ namespace _10.Cube_Properties
     {
         static void Main(string[] args)
         {
+            double readNumber = double.Parse(Console.ReadLine());
+            string readString = Console.ReadLine().ToLower(); //(face, space, volume or area)
+            if (readString == "face")
+            {
+                Console.WriteLine(CubeFace(readNumber));
+            }
+            else if (readString == "space")
+            {
+                Console.WriteLine(CubeSpace(readNumber));
+            }
+            else if (readString == "volume")
+            {
+                Console.WriteLine(CubeVolume(readNumber));
+            }
+            else if (readString == "area")
+            {
+                Console.WriteLine(CubeArea(readNumber));
+            }
+        }
 
+        static string CubeArea(double readNumber)
+        {
+            double result = 6 * Math.Pow(readNumber, 2);
+            return $"{result:F2}";
+        }
+
+        static string CubeVolume(double readNumber)
+        {
+            double result = Math.Pow(readNumber, 3);
+            return $"{result:F2}";
+        }
+
+        static string CubeSpace(double readNumber)
+        {
+            double result = Math.Sqrt(3 * Math.Pow(readNumber, 2));
+            return $"{result:F2}";
+        }
+
+        static string CubeFace(double readNumber)
+        {
+            double result = Math.Sqrt(2 * Math.Pow(readNumber,2));
+            return $"{result:F2}";
         }
     }
 }
