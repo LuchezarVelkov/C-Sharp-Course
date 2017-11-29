787a7c73562e9ce394d7a85b7631bba9b2559de6
LuchezarVelkov <luchezar.velk@gmail.com>
2017-11-30 01:10:04 +0200

    add some homework`s

--- 02 Programming Fundamentals/12 METHODS. DEBUGGING AND TROUBLESHOOTING CODE - EXERCISES/09. Longer Line/09. Longer Line.cs
+++ 02 Programming Fundamentals/12 METHODS. DEBUGGING AND TROUBLESHOOTING CODE - EXERCISES/09. Longer Line/09. Longer Line.cs
@@ -24,28 +24,20 @@ namespace _09.Longer_Line
             double endFirstLineX2, double endFirstLineY2, double startSecondLineX1, double startSecondLineY1,
             double endSecondLineX2, double endSecondLineY2)
         {
-            //double firstHypotenuseStart = Math.Abs(Math.Sqrt(Math.Pow(startFirstLineX1,2) + Math.Pow(startFirstLineY1,2)));
-            //double firstHypotenuseStop = Math.Abs(Math.Sqrt(Math.Pow(endFirstLineX2, 2) + Math.Pow(endFirstLineY2, 2)));
             double firstLine = Math.Sqrt((endFirstLineX2 - startFirstLineX1) * (endFirstLineX2 - startFirstLineX1) +
                 (endFirstLineY2 - startFirstLineY1) * (endFirstLineY2 - startFirstLineY1));
-
-            //double fsecondHypotenuseStart = Math.Abs(Math.Sqrt(Math.Pow(startSecondLineX1, 2) + Math.Pow(startSecondLineY1, 2)));
-            //double secondHypotenuseStop = Math.Abs(Math.Sqrt(Math.Pow(endSecondLineX2, 2) + Math.Pow(endSecondLineY2, 2)));
+            
             double secondLine = Math.Sqrt((endSecondLineX2 - startSecondLineX1) * (endSecondLineX2 - startSecondLineX1) +
                 (endSecondLineY2 - startSecondLineY1) * (endSecondLineY2 - startSecondLineY1));
 
             string result = null;
-            if (firstLine > secondLine)
+            if (firstLine >= secondLine)
             {
                 result = CheckBigerOfTwoLines(startFirstLineX1,startFirstLineY1,endFirstLineX2,endFirstLineY2);
             }
             else if (secondLine > firstLine)
             {
                 result = CheckBigerOfTwoLines(startSecondLineX1,startSecondLineY1,endSecondLineX2,endSecondLineY2);
-            }
-            else
-            {
-                result = $"({startFirstLineX1}, {startFirstLineY1})({endFirstLineX2}, {endFirstLineY2})";
             }
             
             return result;
@@ -56,17 +48,13 @@ namespace _09.Longer_Line
             string result = null;
             double distance1 = Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
             double distance2 = Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2));
-            if (distance1 < distance2)
+            if (distance1 <= distance2)
             {
                 result = $"({x1}, {y1})({x2}, {y2})";
             }
             else if (distance2 < distance1)
             {
                 result = $"({x2}, {y2})({x1}, {y1})";
-            }
-            else
-            {
-                result = $"({x1}, {y1})({x2}, {y2})";
             }
             return result;
         }
