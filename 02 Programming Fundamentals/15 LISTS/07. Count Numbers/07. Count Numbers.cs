2238c72fe6ec5602c507b451f529152395bb2f70
LuchezarVelkov <luchezar.velk@gmail.com>
2017-12-04 19:26:35 +0200

    Add some homework`s

--- 02 Programming Fundamentals/15 LISTS/07. Count Numbers/07. Count Numbers.cs
+++ 02 Programming Fundamentals/15 LISTS/07. Count Numbers/07. Count Numbers.cs
@@ -10,14 +10,50 @@ namespace Count_Numbers
     {
         static void Main(string[] args)
         {
-            List<int> numbers = Console.ReadLine()
-                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            List<int> readNumbers = Console.ReadLine()
+                .Split(' ')
                 .Select(int.Parse)
                 .ToList();
-            foreach (var number in numbers.Distinct().OrderBy(x => x))
-                Console.WriteLine(string.Join(" -> ",
-                    number,
-                    numbers.Count(x => x == number)));
+
+            readNumbers.Sort();
+
+            int index = 0;
+            List<int> numbers = new List<int>();
+            List<int> repeats = new List<int>();
+
+            while (readNumbers.Count >= 1)
+            {
+                int currentNumber = readNumbers[index];
+                numbers.Add(readNumbers[index]);
+                int count = 1;
+                for (int i = index + 1; i < readNumbers.Count; i++)
+                {
+                    if (readNumbers[index] == readNumbers[i])
+                    {
+                        count++;
+                    }
+                }
+                while (readNumbers.Remove(currentNumber))
+                {
+                    readNumbers.Remove(currentNumber);
+                }
+                repeats.Add(count);
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine($"{numbers[i]} -> {repeats[i]}");
+
+            }
+
+            //List<int> numbers = Console.ReadLine()
+            //    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .ToList();
+            //foreach (var number in numbers.Distinct().OrderBy(x => x))
+            //    Console.WriteLine(string.Join(" -> ",
+            //        number,
+            //        numbers.Count(x => x == number)));
         }
     }
 }
