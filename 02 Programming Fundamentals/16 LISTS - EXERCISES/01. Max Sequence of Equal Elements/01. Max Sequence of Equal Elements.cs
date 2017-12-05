d4f81c54393e5dcefe82ef6e362279c3ef35f708
LuchezarVelkov <luchezar.velk@gmail.com>
2017-12-06 00:30:12 +0200

    Add some homework`s

--- 02 Programming Fundamentals/16 LISTS - EXERCISES/01. Max Sequence of Equal Elements/01. Max Sequence of Equal Elements.cs
+++ 02 Programming Fundamentals/16 LISTS - EXERCISES/01. Max Sequence of Equal Elements/01. Max Sequence of Equal Elements.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01.Max_Sequence_of_Equal_Elements
 {
@@ -10,6 +8,42 @@ namespace _01.Max_Sequence_of_Equal_Elements
     {
         static void Main(string[] args)
         {
+            List<int> readNumbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int number = 0;
+            int count = 1;
+            int maxCount = 0;
+
+            for (int i = 0; i < readNumbers.Count -1; i++)
+            {
+                if (readNumbers[i] == readNumbers[i + 1])
+                {
+                    count++;
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        number = readNumbers[i];
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            if (maxCount == 0)
+            {
+                Console.WriteLine(readNumbers[0]);
+            }
+            else
+            {
+                for (int i = 0; i < maxCount; i++)
+                {
+                    Console.Write($"{number} ");
+                }
+            }
         }
     }
 }
